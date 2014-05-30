661ddbdd25bb7c9b1ca45d9a0bc3c761a8d33ec0
liuyun96 <260418992@qq.com>
2014-05-30 18:25:06 +0800

    记录activity回收状态

--- SinaStock/src/com/finance/stock/activity/LoadingActivity.java
+++ SinaStock/src/com/finance/stock/activity/LoadingActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import com.finance.stock.R;
 
@@ -13,9 +14,23 @@ public class LoadingActivity extends BaseActivity {
 	private Handler mHandler = new Handler();
 
 	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// 如果被回收了记录状态
+		outState.putLong("id", 12345678);
+	}
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		if (savedInstanceState != null) {
+			long id = savedInstanceState.getLong("id");
+			Log.e("id", "" + id);
+			onResume();
+			//onStart();
+			return;
+		}
 		super.onCreate(savedInstanceState);
-		//initView();
+		// initView();
 		showMainUI();
 		this.mHandler.postDelayed(this.showMainUI, 100L);
 	}
@@ -26,8 +41,8 @@ public class LoadingActivity extends BaseActivity {
 
 	private void showMainUI() {
 		Intent localIntent = new Intent();
-		//localIntent.setClass(this, MainActivity.class);
-		//localIntent.setClass(this, ViewFlipperActivity.class);
+		// localIntent.setClass(this, MainActivity.class);
+		// localIntent.setClass(this, ViewFlipperActivity.class);
 		localIntent.setClass(this, MoreDateListActivity.class);
 		startActivity(localIntent);
 		finish();
@@ -44,5 +59,5 @@ public class LoadingActivity extends BaseActivity {
 			LoadingActivity.this.showMainUI();
 		}
 	}
-	
+
 }
